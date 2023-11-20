9316160ddbbd63a3809b548fe8315e17a8407fd3
lorenzo <lorenzomojica561@gmail.com>
2023-11-19 20:49:11 -0500

    added css

--- server/seed.py
+++ server/seed.py
@@ -31,7 +31,7 @@ with app.app_context():
                 description="Apex Legends is the award-winning, free-to-play Hero Shooter from Respawn Entertainment. Master an ever-growing roster of legendary characters with powerful abilities, and experience strategic squad play and innovative gameplay in the next evolution of Hero Shooter and Battle Royale.",
                 price=30,
                 quantity=20,
-                image='https://media.contentapi.ea.com/content/dam/apex-legends/images/2020/10/champion-edition/apex-champion-edittion-featured-image.jpg.adapt.crop191x100.628p.jpg',
+                image='https://blog.amigofoods.com/wp-content/uploads/2021/01/mexican-sweet-bread-concha.jpg',
                 bakery_id= 1,
             ),
             Item(
@@ -40,16 +40,43 @@ with app.app_context():
                 description="Dead by Daylight is a multiplayer (4vs1) horror game where one player takes on the role of the savage Killer, and the other four players play as Survivors, trying to escape the Killer and avoid being caught and killed.",
                 price=20,
                 quantity=20,
-                image='https://cdn1.epicgames.com/offer/611482b8586142cda48a0786eb8a127c/EGS_DeadbyDaylight_BehaviourInteractive_S1_2560x1440-a32581cf9948a9a2e24b2ff15c1577c7',
+                image='https://tornadoughalli.com/wp-content/uploads/2022/06/CINNAMON-SUGAR-MINI-DONUTS-2-1.jpg',
                 bakery_id= 1,
             ),
             Item(
-                name="Bolillo",
-                type="Savory",
+                name="Cortadillos",
+                type="Sweet",
                 description="THE NEW FANTASY ACTION RPG. Rise, Tarnished, and be guided by grace to brandish the power of the Elden Ring and become an Elden Lord in the Lands Between.",
                 price=60,
                 quantity=20,
-                image='https://image.api.playstation.com/vulcan/ap/rnd/202110/2000/aGhopp3MHppi7kooGE2Dtt8C.png',
+                image='https://www.maricruzavalos.com/wp-content/uploads/2020/11/cortadillos-mexicanos-recipe.jpg',
+                bakery_id= 1,
+            ),
+            Item(
+                name="Mantecadas",
+                type="Sweet",
+                description="THE NEW FANTASY ACTION RPG. Rise, Tarnished, and be guided by grace to brandish the power of the Elden Ring and become an Elden Lord in the Lands Between.",
+                price=60,
+                quantity=20,
+                image='https://i0.wp.com/chicanoeats.com/wp-content/uploads/2019/03/IMG_6257.jpg?fit=2400%2C2401&ssl=1',
+                bakery_id= 1,
+            ),
+            Item(
+                name="Galleta con Gragea",
+                type="Sweet",
+                description="THE NEW FANTASY ACTION RPG. Rise, Tarnished, and be guided by grace to brandish the power of the Elden Ring and become an Elden Lord in the Lands Between.",
+                price=60,
+                quantity=20,
+                image='https://cdn.vox-cdn.com/thumbor/z6Wl338jfISB5gLh0A5g0-yOJHU=/33x0:566x400/1200x900/filters:focal(33x0:566x400)/cdn.vox-cdn.com/uploads/chorus_image/image/51517059/El_Burrito_Mercado_cookies.0.0.png',
+                bakery_id= 1,
+            ),
+            Item(
+                name="Polvoron",
+                type="Sweet",
+                description="THE NEW FANTASY ACTION RPG. Rise, Tarnished, and be guided by grace to brandish the power of the Elden Ring and become an Elden Lord in the Lands Between.",
+                price=60,
+                quantity=20,
+                image='https://mysliceofmexico.files.wordpress.com/2021/12/019-20191126-polvorones-napolitanos.jpg?w=1024',
                 bakery_id= 1,
             )
 ]
@@ -64,7 +91,7 @@ with app.app_context():
                 name="enzo",
                 username="enzowenzo",
                 email="example@example.com",
-                password_hash="apple561",
+                _password_hash="apple561",
             )
             db.session.add(customer)
 
